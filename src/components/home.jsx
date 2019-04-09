378cf51acc23b6df75199bcec5c4157c76a076e4
Nguyen Ngoc Thanh 20153393 <thanh.nn153393@sis.hust.edu.vn>
2019-04-09 17:57:53 +0700

    add dependence

---
+++ src/components/home.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    state = {}
+    render() {
+        return <div>
+            <h1>Home world</h1>
+        </div>
+    }
+};
